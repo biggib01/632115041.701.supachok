8817440503d4f7a83db61db0f32e129ae48978fa
biggib01 <77378396+biggib01@users.noreply.github.com>
2021-02-10 23:42:14 +0700

    9-2 : week_10

--- fish.java
+++ fish.java
@@ -1,24 +1,7 @@
-package week7;
+package week10;
 
-public class fish extends Pet{
-    String size = "Small";
-    String water = "Fresh water";
-    int swimspeed = 100;
-
-    void isSwimToTheSee(){
-        System.out.println("My fish isn't swim to the see, he is like "+water);
-    }
-
-    void swimfast(){
-        System.out.println("My fish was" +size+ ", and swim speed at " +swimspeed+" km/hr.");
-    }
-
-    public static void main(String[] args) {
-        fish fish = new fish();
-
-        System.out.println("My fish");
-        fish.showPetData("Nemo", "Tuna (Bluefin)", "White", 2);
-        fish.isSwimToTheSee();
-        fish.swimfast();
+public class fish extends Animal {
+    public String roar(){
+        return "...";
     }
 }
